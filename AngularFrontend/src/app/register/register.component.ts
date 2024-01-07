c192d1bfb525d9d5bf26f2d061266bfd2b056301
arunavanew52486 <arunavanew52486@gmail.com>
2024-01-08 01:24:27 +0530

    validations added for login and register

--- AngularFrontend/src/app/register/register.component.ts
+++ AngularFrontend/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 import { RegisterService } from '../services/register.service';
 
@@ -13,29 +14,52 @@ export class RegisterComponent {
 
     constructor(private _httpRegister: RegisterService) {
         this.registerForm = new FormGroup({
-            email: new FormControl(''),
-            mobile: new FormControl(''),
+            email: new FormControl(null, [Validators.required, Validators.email]),
+            mobile: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern(/^[0-9]*$/)]),
             name: new FormGroup({
-                firstName: new FormControl(''),
-                lastName: new FormControl(''),
+                firstName: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Za-z]*$/)]),
+                lastName: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Za-z]*$/)]),
             }),
-            gender: new FormControl(''),
-            age: new FormControl(null),
+            gender: new FormControl(null),
+            age: new FormControl(null, [Validators.min(0), Validators.max(150)]),
             address: new FormGroup({
-                country: new FormControl(''),
-                state: new FormControl(''),
-                pin: new FormControl(''),
-                city: new FormControl(''),
-                landmark: new FormControl(''),
-                area: new FormControl(''),
+                country: new FormControl(null, Validators.pattern(/^[A-Za-z\s]*$/)),
+                state: new FormControl(null, Validators.pattern(/^[A-Za-z\s]*$/)),
+                pin: new FormControl(null, [Validators.minLength(6), Validators.maxLength(6), Validators.pattern(/^[0-9]*$/)]),
+                city: new FormControl(null, Validators.pattern(/^[A-Za-z\s]*$/)),
+                landmark: new FormControl(null, Validators.pattern(/^[A-Za-z\s,]*$/)),
+                area: new FormControl(null, Validators.pattern(/^[A-Za-z\s,.]*$/)),
             }),
-            password: new FormControl('')
+            password: new FormControl(null, Validators.required)
+            // password: new FormControl(null, Validators.compose([Validators.pattern(/^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$-_])[0-9a-zA-Z!@#$-_]*$/)]))
+            // password: new FormControl(null, Validators.compose([Validators.required, Validators.pattern(/^[0-9]*$/), Validators.pattern(/^[a-zA-Z]*$/), this.containsSpecialCharacterValidator]))
         })
     }
 
+    containsSpecialCharacterValidator(): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } | null => {
+          const value = control.value;
+      
+          if (value === null || value === undefined || value === '') {
+            // Return null if the value is empty or undefined
+            return null;
+          }
+      
+          // Define an array of special characters
+          const specialCharacters = ['!', '@', '#', '$', '-', '_'];
+      
+          // Check if any special character is present in the value
+          const containsSpecialCharacter = specialCharacters.some(char => value.indexOf(char) !== -1);
+      
+          // If no special character is found, return an error object
+          return containsSpecialCharacter ? null : { noSpecialCharacter: true };
+        };
+    }
+
     onSubmit() {
-        // console.log(this.registerForm.value)
-        this._httpRegister.login(this.registerForm.value);
-        this.registerForm.reset();
+        console.log(this.registerForm)
+
+        // this._httpRegister.login(this.registerForm.value);
+        // this.registerForm.reset();
     }
 }
