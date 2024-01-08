4f2349a5a6d97761a45389d382f53b604ec522d9
arunavanew <arunavanew51486@gmail.com>
2024-01-08 14:38:38 +0530

    integration done with java backend

--- AngularFrontend/src/app/services/login.service.ts
+++ AngularFrontend/src/app/services/login.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class LoginService {
-    URL = 'http://localhost:3000/users';
+    URL = 'http://localhost:8080/login';
 
     constructor(private _http: HttpClient) {}
 
--- AngularFrontend/src/app/services/register.service.ts
+++ AngularFrontend/src/app/services/register.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class RegisterService {
-    URL = 'http://localhost:3000/users';
+    URL = 'http://localhost:8080/register';
 
     constructor(private _http: HttpClient) {}
 
