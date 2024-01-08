69b2b906d3c95b981b332b0fa28bb6303f2a0d2b
arunavanew <arunavanew51486@gmail.com>
2024-01-08 16:37:17 +0530

    arunava: all fine grained validations added to registration ts and html

--- AngularFrontend/src/app/register/register.component.ts
+++ AngularFrontend/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
@@ -12,25 +13,25 @@ import { RegisterService } from '../services/register.service';
 export class RegisterComponent {
     registerForm: FormGroup;
 
-    constructor(private _httpRegister: RegisterService) {
+    constructor(private _router: Router, private _httpRegister: RegisterService) {
         this.registerForm = new FormGroup({
-            email: new FormControl(null, [Validators.required, Validators.email]),
+            email: new FormControl(null, [Validators.required, Validators.email, Validators.maxLength(150)]),
             name: new FormGroup({
-                firstName: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Za-z]*$/)]),
-                lastName: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Za-z]*$/)]),
+                firstName: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Za-z]*$/), Validators.minLength(2), Validators.maxLength(50)]),
+                lastName: new FormControl(null, [Validators.required, Validators.pattern(/^[A-Za-z]*$/), Validators.minLength(2), Validators.maxLength(50)]),
             }),
             mobile: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern(/^[0-9]*$/)]),
             gender: new FormControl(null),
             age: new FormControl(null, [Validators.min(0), Validators.max(150)]),
             address: new FormGroup({
-                country: new FormControl(null, Validators.pattern(/^[A-Za-z\s]*$/)),
-                state: new FormControl(null, Validators.pattern(/^[A-Za-z\s]*$/)),
-                city: new FormControl(null, Validators.pattern(/^[A-Za-z\s]*$/)),
+                country: new FormControl(null, [Validators.pattern(/^[A-Za-z\s]*$/), Validators.maxLength(25)]),
+                state: new FormControl(null, [Validators.pattern(/^[A-Za-z\s]*$/), Validators.maxLength(25)]),
+                city: new FormControl(null, [Validators.pattern(/^[A-Za-z\s]*$/), Validators.maxLength(25)]),
                 pin: new FormControl(null, [Validators.minLength(6), Validators.maxLength(6), Validators.pattern(/^[0-9]*$/)]),
                 area: new FormControl(null, [Validators.pattern(/^[A-Za-z\s,.]*$/), Validators.maxLength(100)]),
-                landmark: new FormControl(null, Validators.pattern(/^[A-Za-z\s,]*$/)),
+                landmark: new FormControl(null, [Validators.pattern(/^[A-Za-z\s,]*$/), Validators.maxLength(25)]),
             }),
-            password: new FormControl(null, Validators.required)
+            password: new FormControl(null, [Validators.required, Validators.maxLength(50)])
             // password: new FormControl(null, Validators.compose([Validators.pattern(/^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$-_])[0-9a-zA-Z!@#$-_]*$/)]))
             // password: new FormControl(null, Validators.compose([Validators.required, Validators.pattern(/^[0-9]*$/), Validators.pattern(/^[a-zA-Z]*$/), this.containsSpecialCharacterValidator]))
         })
@@ -61,5 +62,6 @@ export class RegisterComponent {
 
         this._httpRegister.login(this.registerForm.value);
         this.registerForm.reset();
+        this._router.navigateByUrl('/');
     }
 }
